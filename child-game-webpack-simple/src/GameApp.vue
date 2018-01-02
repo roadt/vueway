5a02cc33af49abd9ca5f92349139130734b4f937
Frank Tang <roadtang@gmail.com>
2018-01-02 19:03:47 +0800

    child-game-webpack-simple -  state & panel appearance

--- child-game-webpack-simple/src/GameApp.vue
+++ child-game-webpack-simple/src/GameApp.vue
@@ -7,8 +7,13 @@
 </div>
 
 <flash ref="flash"/>
+<div style="display:flex">
+	<button class="new_question"  v-if="false" @click="new_question">再出一题</button>
+</div>
 <h1 class="question-panel" >{{ test.question }}= {{ test.answer }}</h1>
-<input-number-panel v-on:inputed="answered"></input-number-panel>
+<div style="display:flex">
+<input-number-panel v-if="state == 1" style="margin:auto"  v-on:inputed="answered"></input-number-panel>
+</div>
 
 </div>
 </template>
@@ -25,39 +30,45 @@ export default {
 		return {
 			test: {question: "1+1", answer: "?"	},
 			flash: "",
+			state: 1, // answing -1 , answered - 2
 			stats: {
 				right: 0,
 				wrong: 0,
 				number: 0
-			}
+			},
 		}
 	},
 	methods: {
 		answered(v) {
-			
 			this.test.answer = v;
 			this.stats.number ++;
 			//check
 			if (eval(this.test.question) == this.test.answer) {
 				this.stats.right++;
 				this.$refs.flash.succ("答对了！你真棒！再出一题")
+				this.state = 2;
 				
 				
 				//final next question
 				var $this = this;
 				setTimeout(function() {
 					$this.new_question();
-				},1000);
+				},3000);
 			}else{
+				this.state = 2;
 				this.stats.wrong++;
 				this.$refs.flash.error("不对，再试一次？");
+				this.state = 1;
 			}
  		},
 		new_question() {
-			this.test.answer = '?'
-			this.test.question =  this.create_question()
+			if (this.state != 1) {
+				this.test.answer = '?'
+				this.test.question =  this.create_question()
+				this.state = 1;
 			
-			this.$refs.flash.message("试试看？");
+				this.$refs.flash.message("试试看？");
+			}
 		},
 		create_question() {
 			var num = _.random(2,3);
@@ -87,8 +98,18 @@ export default {
 }
 </script>
 <style>
+body {
+//background-image: url("http://img2.3lian.com/2014/f4/100/d/17.jpg");
+//background-image: url("http://www.getjoys.net/sharingSub/1708/trtw1314675919142654411398.jpg");
+
+}
+button.new_question {
+margin: auto;
+font-size: 3em;
+}
+
 .question-panel {
-font-size: 5em;
+font-size: 8em;
 color: blue;
 text-align: center;
 }
